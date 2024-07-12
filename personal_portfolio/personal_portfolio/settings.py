b5a2923898b762a5b9c3fed52df54b11a702ba09
aeron1020 <aeronpaduit20@gmail.com>
2024-07-12 16:48:46 +0800

    additional edits

--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -116,8 +116,8 @@ CORS_ALLOW_HEADERS = [
 AUTH_USER_MODEL = "users.NewUser"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
