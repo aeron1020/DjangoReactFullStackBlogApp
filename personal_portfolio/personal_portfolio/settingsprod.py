33d70c36ebceed03b2262f148ee51f0333d1de70
aeron1020 <aeronpaduit20@gmail.com>
2024-07-13 12:54:52 +0800

    add gitignore im main app with .env

--- personal_portfolio/personal_portfolio/settingsprod.py
+++ personal_portfolio/personal_portfolio/settingsprod.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 from django.conf import settings
-import os
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
