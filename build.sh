8947de75b3715f32a72a92d74673ba937b402521
aeron1020 <aeronpaduit20@gmail.com>
2024-07-23 13:24:59 +0800

    add build run2

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# Activate virtual environment
-source /finalproject/venv/bin/activate
+# Exit immediately if a command exits with a non-zero status
+set -e
 
+# Print commands and their arguments as they are executed
+set -x
+
+# Activate virtual environment
+source ./venv/bin/activate
+
+# Navigate to the frontend directory
 cd frontend
+
+# Install dependencies
+npm install
 
 # Run the build command
 npm run build
