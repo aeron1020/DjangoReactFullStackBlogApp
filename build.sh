59b0f3a62388346d57407ec79d637b31180895f1
aeron1020 <aeronpaduit20@gmail.com>
2024-07-23 13:27:45 +0800

    add build run3

--- build.sh
+++ build.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
-
-# Print commands and their arguments as they are executed
 set -x
 
-# Activate virtual environment
-source ./venv/bin/activate
-
-# Navigate to the frontend directory
+echo "Navigating to the frontend directory..."
 cd frontend
 
-# Install dependencies
+echo "Installing npm dependencies..."
 npm install
 
-# Run the build command
+echo "Running the build command..."
 npm run build
