470c357eed4789d7bc7a825ec99d9ba4c7d7f8ea
aeron1020 <aeronpaduit20@gmail.com>
2024-07-25 09:24:49 +0800

    fixing static files for django admin2

--- personal_portfolio/personal_portfolio/settingsprod.py
+++ personal_portfolio/personal_portfolio/settingsprod.py
@@ -69,7 +69,7 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
