7529fd3e1b2430b1b91dc4d5aadb9ab154b01ff0
aeron1020 <aeronpaduit20@gmail.com>
2024-08-13 18:29:29 +0800

    change user_name to username

--- personal_portfolio/users/models.py
+++ personal_portfolio/users/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class CustomAccountManager(BaseUserManager):
-    def create_superuser(self, email, user_name, first_name, password, **other_fields):
+    def create_superuser(self, email, username, first_name, password, **other_fields):
         
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
@@ -15,16 +15,16 @@ class CustomAccountManager(BaseUserManager):
         if other_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must be assigned to is_superuser=True.')
         
-        return self.create_user(email, user_name, first_name, password, **other_fields)
+        return self.create_user(email, username, first_name, password, **other_fields)
     
 
-    def create_user(self, email, user_name, first_name, password, **other_fields):
+    def create_user(self, email, username, first_name, password, **other_fields):
         
         if not email:
             raise ValueError(_('You must provide an email address'))
         
         email = self.normalize_email(email)
-        user = self.model(email=email, user_name=user_name, first_name=first_name, **other_fields)
+        user = self.model(email=email, username=username, first_name=first_name, **other_fields)
         user.set_password(password)
         user.save()
         return user
@@ -52,7 +52,7 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name', 'first_name']
+    REQUIRED_FIELDS = ['username', 'first_name']
 
     def __str__(self):
         return self.username
