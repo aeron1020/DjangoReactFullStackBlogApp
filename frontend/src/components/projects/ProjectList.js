657c9b3a37901199a25390fc8658e51b6a0a296e
aeron1020 <aeronpaduit20@gmail.com>
2024-08-19 23:37:15 +0800

    add sort in projects

--- frontend/src/components/projects/ProjectList.js
+++ frontend/src/components/projects/ProjectList.js
@@ -1,3 +1,60 @@
+// import React from "react";
+// import {
+//   Container,
+//   List,
+//   ListItemText,
+//   Typography,
+//   Card,
+//   CardActionArea,
+//   CardContent,
+//   useTheme,
+// } from "@mui/material";
+
+// const ProjectList = ({ projects, handleProjectClick, selectedProjectId }) => {
+//   const theme = useTheme();
+
+//   return (
+//     <Container>
+//       <Typography
+//         variant="h2"
+//         mb={2}
+//         color={theme.palette.primary.text}
+//         sx={{ textAlign: "left", paddingLeft: 2, paddingTop: 2 }}
+//       >
+//         Projects List
+//       </Typography>
+
+//       <List sx={{ padding: 1 }}>
+//         {projects.map((project) => (
+//           <Card
+//             key={project.id}
+//             variant="outlined"
+//             sx={{
+//               marginBottom: theme.spacing(1),
+//               backgroundColor:
+//                 project.id === selectedProjectId
+//                   ? theme.palette.action.selected
+//                   : "inherit",
+//               transition: "background-color 0.3s",
+//             }}
+//           >
+//             <CardActionArea onClick={() => handleProjectClick(project.id)}>
+//               <CardContent>
+//                 <ListItemText
+//                   primary={project.project_title}
+//                   style={{ color: theme.palette.primary.text }}
+//                 />
+//               </CardContent>
+//             </CardActionArea>
+//           </Card>
+//         ))}
+//       </List>
+//     </Container>
+//   );
+// };
+
+// export default ProjectList;
+
 import React from "react";
 import {
   Container,
@@ -13,6 +70,11 @@ import {
 const ProjectList = ({ projects, handleProjectClick, selectedProjectId }) => {
   const theme = useTheme();
 
+  // Sort projects by date in descending order (newest first)
+  const sortedProjects = [...projects].sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+
   return (
     <Container>
       <Typography
@@ -25,7 +87,7 @@ const ProjectList = ({ projects, handleProjectClick, selectedProjectId }) => {
       </Typography>
 
       <List sx={{ padding: 1 }}>
-        {projects.map((project) => (
+        {sortedProjects.map((project) => (
           <Card
             key={project.id}
             variant="outlined"
